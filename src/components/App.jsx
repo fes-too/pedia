008ca69381cd124b445225bd13811ee95ae64f32
fes-too <58209794+fes-too@users.noreply.github.com>
2022-06-28 15:07:34 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+//1.create Entry component
+//2.create props to replace hard coded data
+//3a. Import the emojipedia const
+//3.MAp through the emojipedia array and render entry components
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
